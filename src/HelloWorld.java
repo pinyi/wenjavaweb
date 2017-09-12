898dcdce067cb86bf3584f837b5ca2da9535083e
wenjq <1967226721@qq.com>
2017-09-12 14:25:44 +0800

    helleworld add

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
         public static void main(String[] agrs){
             int a = 0;
+            int b = a + 5;
             System.out.println("Hello world!!!");
         }
 }
